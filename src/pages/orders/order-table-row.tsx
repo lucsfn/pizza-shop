6ba12e8decd1565439fce4eb20882fea19fc0991
Lucas Fernandes <lucas.nascimento.1529159@sga.pucminas.br>
2025-08-13 11:44:02 -0300

    feat(orders): implement orders page, with order table and order dialog, ui structure

---
+++ src/pages/orders/order-table-row.tsx
@@ -0,0 +1,49 @@
+import { ArrowRight, Search, X } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+import { TableCell, TableRow } from "@/components/ui/table";
+
+import { OrderDetails } from "./order-details";
+
+export function OrderTableRow() {
+  return (
+    <TableRow>
+      <TableCell>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline" size="xs">
+              <Search className="h-3 w-3" />
+              <span className="sr-only">Detalhes do pedido</span>
+            </Button>
+          </DialogTrigger>
+          <OrderDetails />
+        </Dialog>
+      </TableCell>
+      <TableCell className="font-mono text-sm font-medium">
+        12df45gdf745ds7f
+      </TableCell>
+      <TableCell className="text-muted-foreground">há 30 minutos</TableCell>
+      <TableCell>
+        <div className="flex items-center gap-2">
+          <span className="h-2 w-2 rounded-full bg-slate-400"></span>
+          <span className="text-muted-foreground font-medium">Pendente</span>
+        </div>
+      </TableCell>
+      <TableCell className="font-medium">Lucas Fernandes</TableCell>
+      <TableCell className="font-medium">R$ 132,80</TableCell>
+      <TableCell>
+        <Button variant="outline" size="xs">
+          <ArrowRight className="mr-2 h-3 w-3" />
+          Aprovar
+        </Button>
+      </TableCell>
+      <TableCell>
+        <Button variant="ghost" size="xs">
+          <X className="mr-2 h-3 w-3" />
+          Cancelar
+        </Button>
+      </TableCell>
+    </TableRow>
+  );
+}

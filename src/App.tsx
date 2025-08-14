0d71324b1dd90bb12a3dfed302155ad3483ad578
Lucas Fernandes <lucas.nascimento.1529159@sga.pucminas.br>
2025-08-14 15:44:31 -0300

    feat(api): create axios and react-query config archives and create sign-in request in frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import "@/global.css";
 
+import { QueryClientProvider } from "@tanstack/react-query";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { RouterProvider } from "react-router";
 import { Toaster } from "sonner";
 
 import { ThemeProvider } from "./components/theme/theme-provider";
+import { queryClient } from "./lib/react-query";
 import { router } from "./routes";
 
 export function App() {
   return (
     <HelmetProvider>
       <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
-        <Helmet titleTemplate="%s | pizza.shop" defaultTitle="pizza.shop" />
+        <Helmet titleTemplate="%s | pizza.shop" />
         <Toaster richColors closeButton />
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   );

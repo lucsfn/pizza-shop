96a4bf7de0dc761d445216c89b7d7fbeba1fb442
Lucas Fernandes <lucas.nascimento.1529159@sga.pucminas.br>
2025-08-19 10:55:31 -0300

    feat(profile): logout fn, ui and call to api

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -1,12 +1,14 @@
-import { useQuery } from "@tanstack/react-query";
-import { Building, ChevronDown, LogOut } from "lucide-react";
+import { useMutation, useQuery } from '@tanstack/react-query'
+import { Building, ChevronDown, LogOut } from 'lucide-react'
+import { useNavigate } from 'react-router'
 
-import { getManagedRestaurant } from "@/api/get-managed-restaurant";
-import { getProfile } from "@/api/get-profile";
+import { getManagedRestaurant } from '@/api/get-managed-restaurant'
+import { getProfile } from '@/api/get-profile'
+import { signOut } from '@/api/sign-out'
 
-import { StoreProfileDialog } from "./store-profile-dialog";
-import { Button } from "./ui/button";
-import { Dialog, DialogTrigger } from "./ui/dialog";
+import { StoreProfileDialog } from './store-profile-dialog'
+import { Button } from './ui/button'
+import { Dialog, DialogTrigger } from './ui/dialog'
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -14,22 +16,31 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "./ui/dropdown-menu";
-import { Skeleton } from "./ui/skeleton";
+} from './ui/dropdown-menu'
+import { Skeleton } from './ui/skeleton'
 
 export function AccountMenu() {
+  const navigate = useNavigate()
+
   const { data: profile, isLoading: isLoadingProfile } = useQuery({
-    queryKey: ["profile"],
+    queryKey: ['profile'],
     queryFn: getProfile,
     staleTime: Infinity,
-  });
+  })
 
   const { data: managedRestaurant, isLoading: isLoadingManagedRestaurant } =
     useQuery({
-      queryKey: ["managed-restaurant"],
+      queryKey: ['managed-restaurant'],
       queryFn: getManagedRestaurant,
       staleTime: Infinity,
-    });
+    })
+
+  const { mutateAsync: signOutFn, isPending: isSigningOut } = useMutation({
+    mutationFn: signOut,
+    onSuccess: () => {
+      navigate('/sign-in', { replace: true })
+    },
+  })
 
   return (
     <Dialog>
@@ -37,7 +48,7 @@ export function AccountMenu() {
         <DropdownMenuTrigger asChild>
           <Button
             variant="outline"
-            className="flex items-center gap-2 select-none"
+            className="flex select-none items-center gap-2"
           >
             {isLoadingManagedRestaurant ? (
               <Skeleton className="h-4 w-40" />
@@ -47,6 +58,7 @@ export function AccountMenu() {
             <ChevronDown className="h-4 w-4" />
           </Button>
         </DropdownMenuTrigger>
+
         <DropdownMenuContent align="end" className="w-56">
           <DropdownMenuLabel className="flex flex-col">
             {isLoadingProfile ? (
@@ -57,7 +69,7 @@ export function AccountMenu() {
             ) : (
               <>
                 <span>{profile?.name}</span>
-                <span className="text-muted-foreground text-xs font-normal">
+                <span className="text-xs font-normal text-muted-foreground">
                   {profile?.email}
                 </span>
               </>
@@ -70,13 +82,20 @@ export function AccountMenu() {
               <span>Perfil da loja</span>
             </DropdownMenuItem>
           </DialogTrigger>
-          <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
-            <LogOut className="mr-2 h-4 w-4 text-rose-500 dark:text-rose-400" />
-            <span>Sair</span>
+          <DropdownMenuItem
+            asChild
+            disabled={isSigningOut}
+            className="text-rose-500 dark:text-rose-400"
+          >
+            <button className="w-full" onClick={() => signOutFn()}>
+              <LogOut className="mr-2 h-4 w-4" />
+              <span>Sair</span>
+            </button>
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
+
       <StoreProfileDialog />
     </Dialog>
-  );
+  )
 }

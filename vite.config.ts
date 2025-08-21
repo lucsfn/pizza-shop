bf79e3561a5615cb910e4cd198b303b236b15457
Lucas Fernandes <lucas.nascimento.1529159@sga.pucminas.br>
2025-08-21 14:45:37 -0300

    chore: initial setup for vitest tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import type { UserConfig } from "vite";
 import { defineConfig } from "vite";
+import type { InlineConfig } from "vitest/node";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,4 +13,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  test: {
+    globals: true
+  }
+} as UserConfig & {
+  test: InlineConfig;
 });

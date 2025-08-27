912e276d711bbdf8c1b5af20efc705569ee7011a
Lucas Fernandes <lucasfernandes1506@gmail.com>
2025-08-27 15:02:30 -0300

    test(sign-in): implement e2e sign-in test

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,18 +2,19 @@ import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
   testDir: "./test",
+  testMatch: /.*\.e2e-spec\.ts$/,
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   // reporter: "html",
   use: {
-    baseURL: 'http://localhost:58789',
+    baseURL: "http://localhost:58789",
   },
 
   webServer: {
-    command: 'pnpm dev:test',
-    url: 'http://localhost:58789',
+    command: "pnpm dev:test",
+    url: "http://localhost:58789",
     reuseExistingServer: !process.env.CI,
   },
 
